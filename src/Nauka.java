7160aeaee2e3d44a760fcc5b9ac7b078b3439795
Dorota Szwed <dorota.soroko@gmail.com>
2017-09-13 18:43:26 +0200

    Zmiana 4: dodanie linii kodu "i chę być w tym dobra"

--- src/Nauka.java
+++ src/Nauka.java
@@ -4,6 +4,7 @@ public class Nauka {
         System.out.println("się");
         System.out.println("programowania");
         System.out.println("i commitowania");
+        System.out.println("i chcę być w tym dobra:)");
 
     }
 }
